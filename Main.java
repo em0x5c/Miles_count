ec0581a7ae42628e377facb17b64db8d5bacc655
Elena Mikhaylova <elena__mihailova@mail.ru>
2020-03-09 23:19:19 +0300

    Java code added

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+	    int ticketPrice = 18000;
+	    int miles = ticketPrice / 20;
+        System.out.println("Колличество миль, начисленных за билет стоимостью " + ticketPrice + " руб. составляет "
+                + miles + " миль.");
+    }
+}
